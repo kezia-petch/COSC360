e32cf56d2af2fa365769a181d2fdda653e800a08
kezia-petch <62090035+kezia-petch@users.noreply.github.com>
2020-03-15 21:26:03 +1300

    COSC360

---
+++ Lab01/Planet.cs
@@ -0,0 +1,25 @@
+﻿/*
+	Created by: Lech Szymanski
+				lechszym@cs.otago.ac.nz
+				Oct 27, 2015			
+*/
+
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	// Rate of the 'bob' movement
+	public float bobRate;
+
+	// Scale of the 'bob' movement
+	public float bobScale;
+
+	// Update is called once per frame
+	void Update () {
+		// Change in vertical distance 
+		float dy = bobScale * Mathf.Sin(bobRate * Time.time);
+		
+		// Move the game object on the vertical axis
+		transform.Translate(new Vector3(0, dy, 0));
+	}
+}
---
+++ Lab01/Player.cs
@@ -0,0 +1,36 @@
+﻿/*
+	Created by: Lech Szymanski
+				lechszym@cs.otago.ac.nz
+				Oct 27, 2015			
+*/
+
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public KeyCode moveLeft;
+	public KeyCode moveRight;
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		float delta = speed * Time.deltaTime;
+		
+		if(Input.GetKey(moveRight)) {
+			// Move to the right
+			transform.Translate(new Vector3(speed * delta, 0, 0));	
+		} else if(Input.GetKey(moveLeft)) {
+			// Move to the left
+			transform.Translate(new Vector3(-speed * delta, 0, 0));
+		} else if(Input.GetKey(moveUp)) {
+			//Rotate counterclockwise
+			transform.Rotate( new Vector3(0, 0, speed * delta * 10));
+		} else if(Input.GetKey(moveDown)) {
+			//Rotate clockwise
+			transform.Rotate( new Vector3(0, 0, -speed * delta * 10));
+		}
+	}
+}
